906d7cf1703b1716dcc4da08fcd223d5d21c02a4
Grant McNatt <60593837+ZoOtMcNoOt@users.noreply.github.com>
2025-06-02 15:49:38 -0500

    Refactor main.cpp to implement MAX30105 sensor functionality and update platformio.ini with necessary library dependencies

--- ppg/src/main.cpp
+++ ppg/src/main.cpp
@@ -1,18 +1,65 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include "MAX30105.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include "heartRate.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4;
+byte rates[RATE_SIZE]; 
+byte rateSpot = 0;
+long lastBeat = 0; 
+
+float beatsPerMinute;
+int beatAvg;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  particleSensor.begin(Wire, I2C_SPEED_FAST);
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); 
+  particleSensor.setPulseAmplitudeRed(0x0A); 
+  particleSensor.setPulseAmplitudeGreen(0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; 
+      rateSpot %= RATE_SIZE; 
+
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+
+  Serial.print(">IR:");
+  Serial.println(irValue);
+  Serial.print(">BPM:");
+  Serial.println(beatsPerMinute);
+  Serial.print(">AvgBPM:");
+  Serial.println(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" No finger?");
+
+  Serial.println();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
